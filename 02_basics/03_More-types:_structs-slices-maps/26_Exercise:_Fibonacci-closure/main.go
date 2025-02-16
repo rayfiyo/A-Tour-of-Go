2bff8ee5d66c95849d13273de39a8fe28ed329a0
rayfiyo <rayfiyorex@gmail.com>
2025-02-16 18:32:04 +0900

    refactor: 💡 26 のコメント追加

--- 02_basics/03_More-types:_structs-slices-maps/26_Exercise:_Fibonacci-closure/main.go
+++ 02_basics/03_More-types:_structs-slices-maps/26_Exercise:_Fibonacci-closure/main.go
@@ -5,11 +5,22 @@ import (
 )
 
 // fibonacci is a function that returns a function that returns an int.
+// フィボナッチは int を返す関数を返す関数
 func fibonacci() func() int {
-	n0, n1 := 0, 1 // 変数に代入する一番最初だけ呼び出される
+	// 変数に代入する一番最初だけ呼び出される
+	// "fibonacci関数スコープ" の変数が定義とも言える
+	n0, n1 := 0, 1
+
 	return func() int {
+		// 戻り値に使う値を一時変数vに保持
 		v := n0
+
+		// 次回向けに状態を更新
+		// n1 は次のフィボナッチ数が格納されているので n0 に代入
+		// 次の次（n1の次）の値を計算したものを n1 に代入
+		// n0 = n1 ; n1 = n0 + n1 とは違う．こちらは２の累乗になる
 		n0, n1 = n1, n0+n1
+
 		return v
 	}
 }
